6c8c1514e46c9ef5b78482287eaa5b5bedf29ba7
Zounon <matthewsantos314@gmail.com>
2021-06-23 14:35:16 -0400

    working .gitignore

--- countryroad_frames/save_frames.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-cap = cv2.VideoCapture('test_countryroad.mp4')
-success, image = cap.read()
-count = 0
-while success: 
-    cv2.imwrite("countryroad_frames/frame%d.jpg" % count, image)
-    success, image = cap.read()
-    print('sdf')
-    count += 1
