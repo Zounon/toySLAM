f217d2ec78391221067a4618fa6eaaa1a28761f1
Zounon <matthewsantos314@gmail.com>
2021-06-24 13:32:48 -0400

    stuck again at using keypoints

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os
 import cv2 as cv
+from collections import defaultdict
 from skimage.measure import ransac
 from skimage.transform import EssentialMatrixTransform
 from display import Display2D
@@ -37,15 +38,27 @@ class Camera(object):
         self.K = K
         self.Kinv = np.linalg.inv(K)
         self.F = F
-
+        self.D = None
 
     def undistort_points(self, uvs): 
+        print('undistort_points:', uvs)
         uvs_contiguous = np.ascontiguousarray(uvs[:, :2]).reshape((uvs.shape[0], 1, 2))
         uvs_undistorted = cv.undistortPoints(uvs_contiguous, self.K, self.D, None, self.K)
-        return uvs_undistorted.ravel().reshape(uvs_undistorted.shape[0], 2)
+        ret = uvs_undistorted.ravel().reshape(uvs_undistorted.shape[0], 2)
+        print('post undistort: ', ret)
+        return ret
 
     def unproject_points(self, uvs):
-        return np.dot(self.Kinv, add_ones(uvs).T).T[:, 0:2] 
+        return np.dot(self.Kinv, self.add_ones(uvs).T).T[:, 0:2] 
+
+    # turn [[x,y]] -> [[x,y,1]]
+    def add_ones(self, x):
+        if len(x.shape) == 1:
+            #return np.concatenate([x,np.array([1.0])], axis=0)
+            return np.array([x[0], x[1], 1])
+            #return np.append(x, 1)
+        else:
+            return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
 class FeatureMatcher(object): 
     def __init__(self):
@@ -109,15 +122,28 @@ class Frame(object):
         #     self.annotated_img = self.annotateImg(
         #                             self.img, slam.frames[-2].kps, slam.frames[-1].kps)
 
-    def estimatePose(self, last_kps, curr_kps):
-        self.adj_last_kps = self.cam.unproject_points( self.cam.undistort_points( last_kps) ) 
-        self.adj_curr_kps = self.cam.unproject_points( self.cam.undistort_points( curr_kps) ) 
-        
+    def estimatePose(self, last_frame, curr_frame):
+        self.adj_last_kps = cam.unproject_points( cam.undistort_points( last_frame.kps) ) 
+        self.adj_curr_kps = cam.unproject_points( cam.undistort_points( curr_frame.kps) ) 
+
+        # TODO Might have to set adjusted kps to the normal kps
+        last_frame.kpn = self.adj_last_kps
+        curr_frame.kpn = self.adj_curr_kps
+
         E, self.mask = cv.findEssentialMat(
-                                            self.adj_curr_kps, self.adj_last_kps, focal=1, 
+                                            curr_frame.kpn, last_frame.kpn, focal=1, 
                                             pp=(0., 0.), method=cv.RANSAC, prob=0.999, 
-                                            kRansacThresholdNormalized=0.0003
+                                            threshold=0.0003
                                             )
+        _, R, t, mask = cv.recoverPose(E, curr_frame.kpn, last_frame.kpn, focal=1, pp=(0.,0.))
+        
+        # E, self.mask = cv.findEssentialMat(
+        #                                     self.adj_curr_kps, self.adj_last_kps, focal=1, 
+        #                                     pp=(0., 0.), method=cv.RANSAC, prob=0.999, 
+        #                                     threshold=0.0003
+        #                                     )
+        # _, R, t, mask = cv.recoverPose(E, self.adj_curr_kps, self.adj_last_kps, focal=1, pp=(0.,0.))
+        return R,t
 
     def process_first_frame(self): 
         self.kps, self.des = orb.detectAndCompute(self.img_grey, mask=None)
@@ -127,33 +153,68 @@ class Frame(object):
         self.kps = np.array([x.pt for x in self.kps], dtype=np.float32)
         
         # paint the keypoints and display the result
-        self.drawFeatures()
+        self.drawFeatures(None, None)
     
     def process_next_frame(self, last_frame, curr_frame): 
+        kps_tuples, self.des = orb.detectAndCompute(self.img_grey, mask=None)
+        self.kps = [cv.KeyPoint(*kp) for kp in kps_tuples]
+
+        # self.kps is a list of keypoint objects, we want the numberical (float) values
+        # so we convert list of keypoints to array of points
+        self.kps = np.array([x.pt for x in self.kps], dtype=np.float32)
+
         curr_frame.matches = []
-        curr_frame.matches = cv.knnMatch(last_frame.des, curr_frame.des, k=2) # last des are query, curr des are train
-
-
-
+        curr_frame.matches = slam.feature_matcher.match_des(last_frame.des, curr_frame.des) # last des are query, curr des are train
+        print(len(curr_frame.matches))
         # estimate pose 
-        R, t = self.estimatePose(last_frame.kps, curr_frame.kps)
-        
-        pass
+        R, t = self.estimatePose(last_frame, curr_frame)
+        self.drawFeatures(last_frame, curr_frame) 
 
 
-    def drawFeatures(self): 
+    def drawFeatures(self, last_frame, curr_frame): 
         annotated_img = self.img_rgb.copy()
-        if slam.FIRST_IMAGE == True or True: 
+
+        if slam.FIRST_IMAGE == True: 
             for point in self.kps:
                 a,b = point.ravel()
                 cv.circle(annotated_img, (int(a), int(b)), 5, (0,255,0), -1)
+        elif False: 
+            print(sum(curr_frame.mask))
+            for i,value in enumerate(curr_frame.mask):
+                if value:
+                    print('\n')
+                    print(curr_frame.matches[0][i])
+                    print(curr_frame.adj_curr_kps[i])
+
+
+        elif False:  
+            print(sum(curr_frame.mask))
+            for i,pts in enumerate(zip(curr_frame.adj_last_kps, curr_frame.adj_curr_kps)):
+                # print(i, pts)
+                if curr_frame.mask[i]:
+                    p1, p2 = pts
+                    a,b = p1.ravel()
+                    c,d = p2.ravel()
+                    a, b, c, d = int(a), int(b), int(c), int(d)
+                    print('\t', curr_frame.mask[i], p1, p2, a, b)
+                    # cv.line(annotated_img, (int(a), int(b)), (int(c),int(d)), (0,255,0), 1)
+                    cv.circle(annotated_img, (int(a), int(b)), 5, (0,255,0), -1)
+                    # cv.circle(annotated_img, (a,b), 5, (0,0,255), -1)
+        elif True: 
+            print(len(curr_frame.matches))
+            x, y = curr_frame.matches[0], curr_frame.matches[1]
+                # print(curr_frame.matches)
+            print(len(x), len(y))
+            for x,y in zip(curr_frame.matches[0], curr_frame.matches[1]):
+                cv.circle(annotated_img, (y,x), 5, (0,255,0), -1)
+            print('\n')
         disp2d.paint(annotated_img)
 
 
 
     def compute_kp_des(self, img):
         orb = cv.ORB_create()
-        bf = cv.BFMatcher(cv.NORM_HAMMING)
+        # bf = cv.BFMatcher(cv.NORM_HAMMING)
         kps = orb.detect(img, None)
         kps, des = orb.compute(img, kps)
         pts = np.array([x.pt for x in kps], dtype=np.float32)
@@ -188,14 +249,15 @@ class SLAM(object):
         
         self.FIRST_IMAGE = True
         self.curr_frame, self.last_frame = None, None
+        self.feature_matcher = FeatureMatcher()
 
     def process_frame(self, img):
         # -----------------------------------------
         # Step 1: Capture new frame as a Frame object
         if self.FIRST_IMAGE == True: 
             self.curr_frame = Frame(img)
-            self.FIRST_IMAGE = False 
             self.curr_frame.process_first_frame()
+            self.FIRST_IMAGE = False 
             return 
 
         else: 
